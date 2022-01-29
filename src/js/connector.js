2f72a3b3b14c5273f436a01c97f4695c4655be4f
Preslav <preslav.aleksandrov@protonmail.com>
2022-01-29 21:21:10 +0000

    Add example icon

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,8 +1,21 @@
-console.log('Hello world');
+var WHITE_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg';
+var BLACK_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-black.svg';
+
+var onBtnClick = function (t, opts) {
+  console.log('Someone clicked the button');
+};
 
 window.TrelloPowerUp.initialize({
     'board-buttons': function(t, opts) {
-        return {};
+        return [{ // we can either provide a button that has a callback function
+            icon: {
+              dark: WHITE_ICON,
+              light: BLACK_ICON
+            },
+            text: 'Callback',
+            callback: onBtnClick,
+            condition: 'edit'
+        }];
     }
 
 });
