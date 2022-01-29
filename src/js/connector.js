de7c6fcccdaa782482f1a895751e933154bba06a
Preslav <preslav.aleksandrov@protonmail.com>
2022-01-29 21:25:54 +0000

    print card

--- src/js/connector.js
+++ src/js/connector.js
@@ -2,7 +2,9 @@ var WHITE_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4
 var BLACK_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-black.svg';
 
 var onBtnClick = function (t, opts) {
-  console.log('Someone clicked the button');
+  return t.card('all').then(function(card){
+      console.log(card);
+  });
 };
 
 window.TrelloPowerUp.initialize({
